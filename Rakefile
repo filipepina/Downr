4c58994c259198c4e1b417a50b313843331d7c78
David Rivera <david.r@hostdime.com>
2014-05-08 16:03:26 -0400

    forgot this

--- Rakefile
+++ Rakefile
@@ -1,5 +1,7 @@
 require "bundler/gem_tasks"
 
+require 'rake/testtask'
+
 Rake::TestTask.new do |t|
   t.libs << 'lib/markdown_helper'
   t.test_files = FileList['test/lib/markdown_helper/*_test.rb']
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,3 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require File.expand_path('../lib/markdown_helper.rb', __FILE__)
+require File.expand_path('../../lib/markdown_helper.rb', __FILE__)
