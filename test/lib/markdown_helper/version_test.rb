b290d82f4619a27c1276cc8cc58881fa2fe8d09b
David Rivera <david.r@hostdime.com>
2014-05-08 15:24:12 -0400

    tests\!

---
+++ test/lib/markdown_helper/version_test.rb
@@ -0,0 +1,8 @@
+require_relative '../../test_helper'
+
+describe MarkdownHelper do
+  
+  it "must be defined" do
+    MakrdownHelper::VERSION.wont_be_nil
+  end
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,3 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require File.expand_path('../lib/markdown_helper.rb', __FILE__)
