a9fa1b223acf20de02ab4459df9405f29436b753
David Rivera <david.r@hostdime.com>
2014-05-09 12:01:37 -0400

    moving stuff around

--- lib/generators/markdown_helper/install/install_generator.rb
+++ lib/generators/markdown_helper/install/install_generator.rb
@@ -1,8 +1,8 @@
 module MarkdownHelper 
-  class InitializerGenerator < Rails::Generators::NameBase
+  class InstallGenerator < Rails::Generators::NameBase
     source_root(File.expand_path(File.dirname(__FILE__))
   
-    def copy_initializer_file
+    def copy_files
       copy_file 'markdown_helper.rb', 'config/initializers/markdown_helper.rb'
     end
   end
