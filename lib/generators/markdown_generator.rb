1396ccb9f6cc65d66998138d404e9eaa09a0faaa
David Rivera <david.r@hostdime.com>
2014-05-09 11:31:50 -0400

    trying to fix generator

--- lib/generators/markdown_generator.rb
+++ lib/generators/markdown_generator.rb
@@ -1,6 +1,9 @@
-class MarkdownHelper < Rails::Generators::Base
-  source_root(File.expand_path(File.dirname(__FILE__))
-  def copy_initializer
-    copy_file 'markdown_helper.rb', 'config/initializers/markdown_helper.rb'
+module MarkdownHelper 
+  class InitializerGenerator < Rails::Generators::Base
+    source_root(File.expand_path(File.dirname(__FILE__))
+  
+    def copy_initializer
+      copy_file 'markdown_helper.rb', 'config/initializers/markdown_helper.rb'
+    end
   end
 end
