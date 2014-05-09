7c34066333963a2db6ba802cc90b8cd5bcba01d8
David Rivera <david.r@hostdime.com>
2014-05-09 12:16:53 -0400

    generators are working\!

--- lib/generators/markdown_helper/install/install_generator.rb
+++ lib/generators/markdown_helper/install/install_generator.rb
@@ -1,6 +1,6 @@
 module MarkdownHelper 
   class InstallGenerator < Rails::Generators::NameBase
-    source_root(File.expand_path(File.dirname(__FILE__))
+    source_root File.expand_path('../../../templates', __FILE__)
   
     def copy_files
       copy_file 'markdown_helper.rb', 'config/initializers/markdown_helper.rb'
