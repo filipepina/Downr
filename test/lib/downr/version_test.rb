d0cd843e7812a0539fb6fbb03d8d1828155ab0a9
David Rivera <David.R.Rivera193@gmail.com>
2014-05-13 22:02:33 -0400

    removed old tests

--- test/lib/downr/version_test.rb
+++
@@ -1,8 +0,0 @@
-require_relative '../../test_helper'
-
-describe Downr do
-  
-  it "must be defined" do
-    Downr::VERSION.wont_be_nil
-  end
-end
--- test/test_helper.rb
+++
@@ -1,4 +0,0 @@
-require 'minitest/autorun'
-require 'minitest/pride'
-require File.expand_path('../../lib/downr.rb', __FILE__)
-require File.expand_path('../../lib/generators/templates/downr',__FILE__)
