e15bac2c554cf7a82a756d65103505cedb0c1b42
David <David.R.Rivera193@gmail.com>
2014-05-16 11:40:25 -0400

    Update railtie.rb
    fixed spacing

--- lib/downr/railtie.rb
+++ lib/downr/railtie.rb
@@ -2,11 +2,11 @@ module Downr
 
   ##
   # Lets us tie up a Rails view helper
-	class Railtie < ::Rails::Railtie
+  class Railtie < ::Rails::Railtie
     initializer "downr.configure_view_controller" do |app|
       ActiveSupport.on_load :action_view do
         ActionView::Base.send :include, Helpers
       end
     end
-	end
+  end
 end
